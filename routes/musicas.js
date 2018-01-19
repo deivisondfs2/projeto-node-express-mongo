083a17a9124287251a950743bd98df10dbf026a5
dfs2 <deivison.francisco@pitang.com>
2018-01-19 09:39:57 -0300

    adicionando rotas para edit e delete

--- routes/musicas.js
+++ routes/musicas.js
@@ -33,11 +33,25 @@ router.get('/edit/:id', (req,res,next)=>{
 
 
 router.put('/edit/:id',	(req,res,next)=>{
-
+    const	{id}	=	req.params;
+    mongoose.model('Musica').update({_id:id},
+    {
+        $set:	
+        {
+            nome:	req.body.nome,
+            artista:	req.body.artista,
+            estrelas:	req.body.estrelas
+        }
+    }).then((musica)	=>	{
+        res.redirect('/')
+    },next);
 });
 
-router.delete('/delete/:id', (req,res,next)=>{
-
+router.get('/delete/:id', (req,res,next)=>{
+    const	{id}	=	req.params;
+    mongoose.model('Musica').remove({_id:	id}).then((musica)	=>	{
+            res.redirect('/')
+    },next);
 });
 
 module.exports = router;
